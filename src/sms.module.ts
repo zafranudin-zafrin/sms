92c9209328ca3766b34a1f131d8c901b0d340e9a
Zafranudin Zafrin <zafranudin.zafrin@carsome.my>
2019-09-24 14:16:47 +0800

    Fix modular issue

--- src/sms.module.ts
+++ src/sms.module.ts
@@ -6,7 +6,25 @@ import {TwilioSmsService} from './dialect/twilio-sms/twilio-sms.service';
 import {LocalSmsService} from './dialect/local-sms/local-sms.service';
 import {BaseSmsService} from './dialect/base-sms.service';
 
-@Module({})
+@Module({
+	imports: [
+		HttpModule,
+	],
+	providers: [
+		SmsService,
+		TwilioSmsService,
+		InfobipSmsService,
+		LocalSmsService,
+		BaseSmsService,
+	],
+	exports: [
+		SmsService,
+		TwilioSmsService,
+		InfobipSmsService,
+		LocalSmsService,
+		BaseSmsService,
+	],
+})
 export class SmsModule {
 	static register(options: SmsOptions): DynamicModule {
 		return {
--- src/sms.service.ts
+++ src/sms.service.ts
@@ -1,25 +1,30 @@
 import {Inject, Injectable} from '@nestjs/common';
 import {SmsOptions} from './interfaces/sms.options';
-import {BaseSmsService} from './dialect/base-sms.service';
+import {InfobipSmsService} from './dialect/infobip-sms/infobip-sms.service';
+import {TwilioSmsService} from './dialect/twilio-sms/twilio-sms.service';
+import {LocalSmsService} from './dialect/local-sms/local-sms.service';
 
 @Injectable()
 export class SmsService {
-	sms: BaseSmsService;
+	sms: any;
 
 	constructor(
 		@Inject('SMS_DIALECT') private readonly SMS_DIALECT: any,
 		@Inject('SMS_OPTIONS') private readonly smsOptions: SmsOptions,
+		private infobip: InfobipSmsService,
+		private twilio: TwilioSmsService,
+		private mock: LocalSmsService,
 	) {
 		this.sms = this.SMS_DIALECT;
 		switch (smsOptions.dialect) {
 			case 'infobip':
-				this.sms = SMS_DIALECT.infobip;
+				this.sms = infobip;
 				break;
 			case 'twilio':
-				this.sms = SMS_DIALECT.twilio;
+				this.sms = twilio;
 				break;
 			default:
-				this.sms = SMS_DIALECT.mock;
+				this.sms = mock;
 				break;
 		}
 

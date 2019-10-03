f0bc5bf7b1b742e5954bb45016322159653ae1e1
Zafranudin Zafrin <zafranudin.zafrin@carsome.my>
2019-10-03 10:47:18 +0800

    Added tests

--- src/specs/c-sms.service.spec.ts
+++ src/specs/c-sms.service.spec.ts
@@ -2,11 +2,39 @@ import {Test, TestingModule} from '@nestjs/testing';
 import {HttpModule, HttpService} from '@nestjs/common';
 import {CSmsService} from '../c-sms.service';
 import {InfobipSmsService} from '../dialect/infobip-sms/infobip-sms.service';
+import {of} from 'rxjs';
+import {AxiosResponse} from 'axios';
 
-describe('LoansService', () => {
+describe('CSmsService', () => {
 	let service: CSmsService;
+	let http: HttpService;
+
+	const message = 'Hello world';
+	const recipientNo = '60123456789';
+	const sender = 'ACME';
+	const result: AxiosResponse = {
+		data: {
+			messages: [{
+				to: recipientNo,
+				status: {
+					groupId: 1,
+					groupName: 'PENDING',
+					id: 26,
+					name: 'PENDING_ACCEPTED',
+					description: 'Message sent to next instance',
+				},
+				messageId: '1568961198322110598',
+			}],
+		},
+		status: 200,
+		statusText: 'OK',
+		headers: {},
+		config: {},
+	};
 
 	beforeEach(async () => {
+		http = new HttpService();
+
 		const module: TestingModule = await Test.createTestingModule({
 			imports: [
 				HttpModule,
@@ -17,19 +45,17 @@ describe('LoansService', () => {
 					provide: 'SMS_OPTIONS',
 					useValue: {
 						dialect: 'infobip',
-						sender: 'test',
+						sender,
 					},
 				},
 				{
 					provide: 'SMS_DIALECT',
-					useFactory: (
-						httpService: HttpService) => {
-						return new InfobipSmsService(httpService, {
+					useFactory: () => {
+						return new InfobipSmsService(http, {
 							dialect: 'infobip',
-							sender: 'test',
+							sender,
 						});
 					},
-					inject: [HttpService],
 				},
 			],
 		}).compile();
@@ -39,5 +65,44 @@ describe('LoansService', () => {
 
 	it('should be defined', () => {
 		expect(service).toBeDefined();
+	});
+
+	it('should send SMS', async () => {
+		jest.spyOn(http, 'post').mockImplementation((): any => {
+			return of(result);
+		});
+
+		expect(await service.send(message, recipientNo)).toBe(result);
+	});
+
+	it('can draft message', async () => {
+
+		expect(await service.draft(message, recipientNo)).toBe(service);
+	});
+
+	it('can submit drafted message', async () => {
+		jest.spyOn(http, 'post').mockImplementation((): any => {
+			return of(result);
+		});
+		const flow = await service.draft(message, recipientNo);
+		expect(await flow.submit()).toBe(result);
+	});
+
+	it('can get the body', async () => {
+		const flow = await service.draft(message, recipientNo);
+
+		expect(flow.getBody()).toStrictEqual({
+			from: sender,
+			to: recipientNo,
+			text: message,
+		});
+	});
+
+	it('can get the response after sent', async () => {
+		jest.spyOn(http, 'post').mockImplementation((): any => {
+			return of(result);
+		});
+		await service.send(message, recipientNo);
+		expect(service.getResponse()).toBe(result);
 	});
 });

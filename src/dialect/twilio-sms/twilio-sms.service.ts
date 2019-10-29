7c8c5398c7a00404779c57c360261d3c4b1b472a
Zafranudin Zafrin <zafranudin.zafrin@carsome.my>
2019-10-29 10:32:47 +0800

    Fix: Type-hinted twilio service

--- src/dialect/twilio-sms/twilio-sms.service.ts
+++ src/dialect/twilio-sms/twilio-sms.service.ts
@@ -1,16 +1,17 @@
 import {Injectable} from '@nestjs/common';
 import {BaseSmsService} from '../base-sms.service';
 import {SmsInterface} from '../../interfaces';
-import * as twilio from 'twilio';
+import {Twilio} from 'twilio';
 
 @Injectable()
 export class TwilioSmsService extends BaseSmsService implements SmsInterface {
-	twilio: twilio;
 	async send(): Promise<any> {
-		const accountSid = process.env.TWILIO_SID;
-		const authToken = process.env.TWILIO_TOKEN;
-		this.twilio = this.twilio(accountSid, authToken);
-		console.log('Method not implemented');
+		const accountSid = String(process.env.TWILIO_SID);
+		const authToken = String(process.env.TWILIO_TOKEN);
+		const twilio: Twilio = new Twilio(accountSid, authToken);
+
+		// Perform twilio action here
+
 		return true;
 	}
 }

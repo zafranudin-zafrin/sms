418850d6f29311f05fd28b4ea48593f22d4e9977
Zafranudin Zafrin <zafranudin.zafrin@carsome.my>
2019-10-29 09:49:20 +0800

    Feature: Implement twilio

--- src/dialect/twilio-sms/twilio-sms.service.ts
+++ src/dialect/twilio-sms/twilio-sms.service.ts
@@ -1,11 +1,15 @@
 import {Injectable} from '@nestjs/common';
 import {BaseSmsService} from '../base-sms.service';
 import {SmsInterface} from '../../interfaces';
+import * as twilio from 'twilio';
 
 @Injectable()
 export class TwilioSmsService extends BaseSmsService implements SmsInterface {
+	twilio: twilio;
 	async send(): Promise<any> {
-		// tslint:disable-next-line:no-console
+		const accountSid = process.env.TWILIO_SID;
+		const authToken = process.env.TWILIO_TOKEN;
+		this.twilio = this.twilio(accountSid, authToken);
 		console.log('Method not implemented');
 		return true;
 	}

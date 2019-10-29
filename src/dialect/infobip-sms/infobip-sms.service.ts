df58854c7d347aa4a4a0eefca85b0b7cfccfc36c
Zafranudin Zafrin <zafranudin.zafrin@carsome.my>
2019-10-29 09:50:37 +0800

    Fix: Add prefix for mobile number

--- src/dialect/infobip-sms/infobip-sms.service.ts
+++ src/dialect/infobip-sms/infobip-sms.service.ts
@@ -14,10 +14,37 @@ export class InfobipSmsService extends BaseSmsService implements SmsInterface {
 		headers: this._headers,
 	};
 
+	get to() {
+		return this._body.to;
+	}
+
+	set to(recipients) {
+		this._body.to = this._validateNumber(recipients);
+	}
+
 	async send(): Promise<any> {
 		this.response = await this.httpService.post(process.env.INFOBIP_BASE_URL + '/sms/2/text/single',
 			this._body,
 			this._config).toPromise();
 		return this.response;
+	}
+
+	private _validateNumber(mobileNo: string | string[]) {
+		let sanitized;
+		if (Array.isArray(mobileNo)) {
+			sanitized = [];
+			for (const num of mobileNo) {
+				sanitized.push(this._cleanNumber(num));
+			}
+		}
+		sanitized = this._cleanNumber(mobileNo);
+		return sanitized;
+	}
+
+	private _cleanNumber(mobileNo) {
+		if (mobileNo.charAt(0) !== '6' && mobileNo.charAt(0) === '0') {
+			mobileNo = '6' + mobileNo;
+		}
+		return mobileNo;
 	}
 }
